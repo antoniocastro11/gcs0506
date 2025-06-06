4f1d2cb825f2ff0bd0ef77ae3005a1c0aefd8ce5
castro <antoniog_castro@hotmail.com>
2025-06-05 23:19:53 -0300

    fix: docstring

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ testes de unidade app
 from app import soma
 
 def test_soma():
-    #função de teste para o arquivo app
+    """ função de teste para o arquivo app"""
     assert soma(2, 3) == 5
 
 def test_main():
